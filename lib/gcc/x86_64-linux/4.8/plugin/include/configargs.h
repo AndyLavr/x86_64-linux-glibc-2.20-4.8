1c40fd1328fa784708a36263984ef451b0d9c84d
frap129 <frap129@gmail.com>
2015-02-05 21:04:24 -0500

    Fix libstdc++ error

---
+++ lib/gcc/x86_64-linux/4.8/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/tmp/syrup-man-gcc-64-xyjM43/toolchain-src/gcc/gcc-4.8/configure --enable-multiarch --with-arch-32=i686 --with-abi=m64 --prefix=/tmp/syrup-man-gcc-64-xyjM43/1/x86_64-linux-glibc2.20-4.8 --with-sysroot=/tmp/syrup-man-gcc-64-xyjM43/1/x86_64-linux-glibc2.20-4.8/sysroot --disable-nls --with-gmp=/tmp/syrup-man-gcc-64-xyjM43/1/x86_64-linux-glibc2.20-4.8 --with-mpfr=/tmp/syrup-man-gcc-64-xyjM43/1/x86_64-linux-glibc2.20-4.8 --with-mpc=/tmp/syrup-man-gcc-64-xyjM43/1/x86_64-linux-glibc2.20-4.8 --target=x86_64-linux --with-arch=x86-64 --with-multilib-list=m32,m64 --disable-plugins --disable-docs --disable-bootstrap --disable-libgomp --disable-libmudflap --disable-libquadmath --enable-target-optspace --enable-gold=default --enable-languages=c,c++ --enable-graphite --with-cloog=/tmp/syrup-man-gcc-64-xyjM43/1/x86_64-linux-glibc2.20-4.8";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "m64" }, { "cpu", "generic" }, { "arch", "x86-64" }, { "arch_32", "i686" } };
